33d50f4d754b4694edad19de75f28718e60ea58e
ugerpaul17 <ugerpaul5@gmail.com>
2020-03-20 12:58:07 +0300

    update the get axios call

--- bootstrap/js/Axios.js
+++ bootstrap/js/Axios.js
@@ -1,23 +1,23 @@
 //Add an event listener for the form submission to create a new user
 
-const form = document.querySelector('');
+const form = document.querySelector('form');
 
 
-const formEvent = form.addEventListener('', event => {
+const formEvent = form.addEventListener('submit', event => {
    event.preventDefault(); 
 
-   const name = document.querySelector('').Value;
-   const email = document.querySelector('').Value;
-   const message = document.querySelector('').Value;
+   const username = document.querySelector('#username').Value;
+   const email = document.querySelector('#email').Value;
+   const message = document.querySelector('#message').Value;
 
-   const user = { name, email, message };
+   const user = { username, email, message };
 
    createUser(user);
 });
 
 
 const createUser = (user) => {
-    axios.post('')
+    axios.post(' http://34.90.92.86:8887/users/')
     .then(response => {
         const addedUser = response.data;
         console.log(`POST: user is added`, addedUser);
@@ -27,32 +27,32 @@ const createUser = (user) => {
     .catch(error => console.error(eror));
 }
 
-//getting the articles from an endpoint and passing it into the DOM
+// //getting the articles from an endpoint and passing it into the DOM
 
-const createList = (articles) => {
-    const li = document.createElement('li');
-    //add articles to the 'li'
-    li.textContent = `${articles.title}: ${articles.content}: ${articles.tag}`;
-    return li;
+// const createList = (articles) => {
+//     const li = document.createElement('li');
+//     //add articles to the 'li'
+//     li.textContent = `${articles.title}: ${articles.content}: ${articles.tag}`;
+//     return li;
 
-};
+// };
 
-const appendToDOM = (articles) => {
-    const ul = document.querySelector('');
-    //iteratw over all articles 
-    articles.map(articles => {
-        ul.appendChild(createList(articles));
-    });
-};
+// const appendToDOM = (articles) => {
+//     const ul = document.querySelector('');
+//     //iteratw over all articles 
+//     articles.map(articles => {
+//         ul.appendChild(createList(articles));
+//     });
+// };
 
 //getting articles from an endpoint 
 const getArticles = () => {
-    axios.get('')
+    axios.get('http://34.90.92.86:8887/articles/')
     .then(response => {
-        const articles = response.data.data;
+        const articles = response.data;
         console.log(`GET List articles`, articles);
         //append to DOM
-        appendToDOM(articles);
+        //appendToDOM(articles);
     })
 
     .catch(error => console.error(error));
