8925a41e96696cdab0ddc203859a5782322e1e5b
ugerpaul17 <ugerpaul5@gmail.com>
2020-04-05 02:58:57 +0300

    picking data from check boxes

--- bootstrap/js/contact.js
+++ bootstrap/js/contact.js
@@ -8,15 +8,39 @@ const formEvent = form.addEventListener('submit', event => {
    const username = document.querySelector("#username").value;
    const email = document.querySelector("#email").value;
    const message = document.querySelector("#message").value;
+ 
 
-   const user = { username, email, message };
+    //get all checked elements 
+    let checkedElement = document.querySelectorAll('.input-contact:checked');
+    let checkedElementValue = [];
+    //loop through all checked element
+    checkedElement.forEach(function(element){
+      checkedElementValue.push(element.value);
+    });
+    if(checkedElementValue.length === 0) {
+      console.log('No item checked')
+    }
+    else { 
+      checkedElementValue
+    }
+
+  
+
+
+   const user = { username, email,  checkedElementValue,message };
+  //  const user2 = {checkedElementValue};
+  
 
    createUser(user);
 });
 
 
 
+
 const createUser = (user) => {
+  console.log("sngrgusus",user);
+ // console.log("hafbauja",JSON.stringify(user2));
+
     let postObject = {...user}
 axios({
     method: 'post',
@@ -24,7 +48,8 @@ axios({
     data: {
       "username": `${postObject.username}`,
       "email": `${postObject.email}`,
-      "message": `${postObject.message}`
+      "message": `${postObject.message}`,
+     // "checkedElementValue": `${postObject.}`
     }
   });
 
